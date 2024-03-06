2f06d78fc47e1aa9d7023369ef4c6448ada906ad
NmnSach <nssachdeva2@gmail.com>
2024-03-06 20:44:03 +0530

    added container scroll

--- app/page.js
+++ app/page.js
@@ -2,13 +2,19 @@
 
 import DarkMode from "@/components/ui/darkmode";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  ContainerScroll,
+  Header,
+} from "@/components/ui/container-scroll-animation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className="text-5xl">ShadCN portfolio</h1>
-     <Button variant='default'>This is a button</Button>
-     <DarkMode  />
-    </main>
+    <div>
+
+      <ContainerScroll />
+      <Header />
+      
+    </div>
   );
 }
